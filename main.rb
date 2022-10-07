266e764f23857767b0a9262f540171e85f58694f
Micah Stalberg <stalberm@oregonstate.edu>
2022-10-07 14:47:54 -0700

    Further updates comments

--- main.rb
+++ main.rb
@@ -2,18 +2,15 @@
 
 @name = "Johanna Jackson"
 
-# Mangle the name by reversing it
+#Reverses the inputted name
 def mangle(name)
-  # Split the name into first and last in an array
   split_name = name.split
-  # Reverse the array of name components so the array is [last, first]
   reversed_name_components = split_name.reverse
-  # Rejoin the array of name components
   name = reversed_name_components.join(' ')
   return name
 end
 
-# Borigy the name by reversing it and adding borg
+#Mangles the name and adds borg to the end
 def borg_mangle(name)
     name = mangle(name)
     name << " Borg"
